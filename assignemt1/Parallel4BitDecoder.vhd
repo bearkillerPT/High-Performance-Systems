46cef7930540a324194ef64a0260cb6967cdbecd
BearkillerPT <gilteixeira@ua.pt>
2022-11-12 14:42:39 +0000

    new project files

--- assignemt1/Parallel4BitDecoder.vhd
+++ assignemt1/Parallel4BitDecoder.vhd
@@ -1,21 +1,17 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY Parallel4BitEncoder IS
-  PORT (m: IN STD_LOGIC_VECTOR(3 downto 0);
-        x:   OUT STD_LOGIC_VECTOR(7 downto 0));
-END Parallel4BitEncoder;
+ENTITY Parallel4BitDecoder IS
+  PORT (x: IN STD_LOGIC_VECTOR(7 downto 0);
+        m:   OUT STD_LOGIC_VECTOR(3 downto 0));
+END Parallel4BitDecoder;
 
-ARCHITECTURE structure OF Parallel4BitEncoder IS
-SIGNAL s0, s1, s2, s3, s4, s5, s6, s7: STD_LOGIC;
+ARCHITECTURE structure OF Parallel4BitDecoder IS
+
 BEGIN
-	s7 <= m(0);
-	s6 <= m(0) xor m(3);
-	s5 <= m(0) xor m(2);
-	s4 <= s5 xor m(3);
-	s3 <= m(0) xor m(1);
-	s2 <= s3 xor m(3);
-	s1 <= s3 xor m(2);
-	s0 <= s1 xor m(3);
-	x <= s7 & s6 & s5 & s4 & s3 & s2 & s1 & s0;
+  
+
 END structure;
+
+ENTITY 
+
